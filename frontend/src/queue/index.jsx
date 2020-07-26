15dd7cf532f555bd1a4d7a66d9de25f012fc544e
Andrew Hsu <nothingwithheldsocial@gmail.com>
2020-07-26 00:37:31 -0500

    Grab dining halls from server

--- frontend/src/queue/index.jsx
+++ frontend/src/queue/index.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QueueSelect from "./queueSelect";
 import QueueRequest from "./queueRequest";
 import QueueDisplay from "./queueDisplay";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 const dummyDiningHalls = ["PAR", "FAR", "BAR", "CAR"];
 const dummyDiningHallQueueSize = [3, 8, 4, 2];
 var dummyDiningHallDict = {
-  "PAR" : 3,
-  "FAR" : 8,
-  "BAR" : 4,
-  "CAR" : 2
+  PAR: 3,
+  FAR: 8,
+  BAR: 4,
+  CAR: 2,
 };
 
 // example list, need to define how it'll look like when grabbed from DB
 // also should use some random UID for the ID component
 const list = [
   {
-    id: 'a',
-    firstname: 'Robin',
-    lastname: 'Wieruch',
+    id: "a",
+    firstname: "Robin",
+    lastname: "Wieruch",
   },
   {
-    id: 'b',
-    firstname: 'Dave',
-    lastname: 'Davidds',
+    id: "b",
+    firstname: "Dave",
+    lastname: "Davidds",
   },
 ];
 
@@ -37,36 +37,43 @@ const list = [
 // }
 
 const Queue = () => {
-  const [diningHalls, setDiningHalls] = useState(Object.keys(dummyDiningHallDict));
+  const [diningHalls, setDiningHalls] = useState([]);
   const [selectedDiningHall, setSelectedDiningHall] = useState("");
-  const [queueSize, setQueueSize] = useState(null);
+  const [queueSize, setQueueSize] = useState();
   // const [groupNetIds, setGroupNetIds] = useState([]);
-  const [diningHallCurrentStatus, updateDiningHallStatus] = useState(
-    {
-      "PAR" : [],
-      "FAR" : [],
-      "BAR" : [],
-      "CAR" : []
-    }
-  );
+  const [diningHallCurrentStatus, updateDiningHallStatus] = useState({
+    PAR: [],
+    FAR: [],
+    BAR: [],
+    CAR: [],
+  });
 
+  useEffect(() => {
+    fetch("http://localhost:3000/dev/dining-hall", {
+      headers: {
+        Authorization: `Basic ${btoa("DeveloperOnly:ajhsu2")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(({ diningHalls }) => {
+        setDiningHalls(diningHalls.map(({ DiningHallName }) => DiningHallName));
+      });
+  }, []);
 
   const handleSelect = (event) => {
     setSelectedDiningHall(event.target.value);
-    setQueueSize(dummyDiningHallDict[event.target.value]);
+    setQueueSize(7);
   };
   const updateDHCallback = (groupNetIds) => {
-    updateDiningHallStatus((prevState)  => (
-      {...prevState, 
-        [selectedDiningHall]: [...prevState[selectedDiningHall], groupNetIds]        
-    }
-    ));
-    console.log(diningHallCurrentStatus);
-  }
+    updateDiningHallStatus((prevState) => ({
+      ...prevState,
+      [selectedDiningHall]: [...prevState[selectedDiningHall], groupNetIds],
+    }));
+  };
 
   return (
     <>
-    <Grid container spacing={3}>
+      <Grid container spacing={3}>
         <Grid item xs={8}>
           <QueueSelect
             diningHalls={diningHalls}
@@ -75,18 +82,15 @@ const Queue = () => {
             queueSize={queueSize}
           />
           <hr style={{ width: "80%" }}></hr>
-          <QueueRequest 
-            updateDHCallback={updateDHCallback}
-          />
+          <QueueRequest updateDHCallback={updateDHCallback} />
         </Grid>
         <Grid item xs={4}>
-          <QueueDisplay 
+          {/* <QueueDisplay
             list={list}
             selectedDiningHall={selectedDiningHall}
             DHGroups={diningHallCurrentStatus[selectedDiningHall]}
-            />
-        </Grid>      
-        
+          /> */}
+        </Grid>
       </Grid>
     </>
   );
