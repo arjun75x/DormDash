7fbf79a1da3c87f6c11dc198ee0797dc65b3fbe3
Arjun Arun <arjun75x@gmail.com>
2020-07-25 20:50:04 -0500

    Create sql folder and add query to initialize db

---
+++ server/src/sql/init_db.sql
@@ -0,0 +1,83 @@
+create table DiningHall(
+
+DiningHallName VARCHAR(255) PRIMARY KEY,
+
+Latitude REAL NOT NULL,
+
+Longitude REAL NOT NULL
+
+)
+
+create table DiningHallTable(
+
+TableID INT PRIMARY KEY,
+
+Capacity INT NOT NULL,
+
+DiningHallName VARCHAR(255) NOT NULL,
+
+FOREIGN KEY (DiningHallName) REFERENCES DiningHall(DiningHallName)
+
+ON UPDATE CASCADE
+
+ON DELETE CASCADE
+
+)
+
+create table QueueRequest(
+
+QueueRequestID INT PRIMARY KEY,
+
+EnterQueueTime DATE NOT NULL,
+
+ExitQueueTime DATE NOT NULL,
+
+DistanceEstimate REAL NOT NULL,
+
+Preferences VARCHAR(255) NOT NULL,
+
+Canceled BOOLEAN NOT NULL,
+
+DiningHallName VARCHAR(255) NOT NULL,
+
+FOREIGN KEY (DiningHallName) REFERENCES DiningHall(DiningHallName)
+
+ON UPDATE CASCADE
+
+ON DELETE CASCADE
+
+)
+
+create table AdmittedEntry(
+
+EntryID INT PRIMARY KEY,
+
+MealType VARCHAR(255) NOT NULL,
+
+AdmitOffQueueTime DATE NOT NULL,
+
+GroupArrivalTime DATE NOT NULL,
+
+GroupExitTime DATE NOT NULL,
+
+DistanceEstimate REAL NOT NULL,
+
+QueueRequestID INT NOT NULL,
+
+FOREIGN KEY (QueueRequestID) REFERENCES QueueRequest(QueueRequestID)
+
+ON UPDATE CASCADE
+
+ON DELETE CASCADE
+
+)
+
+create table User(
+
+NetID VARCHAR(255) PRIMARY KEY,
+
+DeviceID INT NOT NULL,
+
+Name VARCHAR(255) NOT NULL
+
+)
