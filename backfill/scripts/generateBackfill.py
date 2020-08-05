82d2b5132afc25542083691ebe075b8188f8f0b0
Arjun Arun <arjun75x@gmail.com>
2020-08-04 22:15:35 -0500

    Update backfill script

--- backfill/scripts/generateBackfill.py
+++ backfill/scripts/generateBackfill.py
@@ -1,2 +1,18 @@
 import requests
+import random
+import time
+import datetime
+from requests.auth import HTTPBasicAuth
 
+url = 'http://localhost:3000/dev/queueBF'
+current_time = int(time.time())
+
+for i in range(5):
+
+    body = {
+        "DiningHallName": random.choice(["FAR", "Ike", "ISR", "LAR"]),
+        "QueueGroup": random.choices(["tincher2", "arjunsa2", "naymanl2", "ajhsu2"], k=random.randint(1,4)),
+        "joinTime": (datetime.datetime.now() - datetime.timedelta(seconds=random.randint(0, (7 * 24 * 60 * 60)))).isoformat()
+    }
+    x = requests.post(url, json=body, auth=("DeveloperOnly", "arjunsa2"))
+    print(x.json())
