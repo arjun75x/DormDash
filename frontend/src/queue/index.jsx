c9cf98cb4d631a8e8c83691b158885f521dd6271
unknown <nothingwithheldsocial@gmail.com>
2020-08-04 20:18:54 -0500

    Fix timeout clear

--- frontend/src/queue/index.jsx
+++ frontend/src/queue/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import QueueSelect from "./queueSelect";
 import QueueRequest from "./queueRequest";
@@ -24,14 +24,55 @@ const Queue = ({
   const [diningHalls, setDiningHalls] = useState([]);
   const [selectedDiningHall, setSelectedDiningHall] = useState("");
   const [queueSize, setQueueSize] = useState(null);
-  const [queueTimeoutId, setQueueTimeoutId] = useState(null);
+  const queueSizeTimeoutId = useRef(null);
   const [queueReqResponse, setQueueReqResponse] = useState({});
+  const queueReqTimeoutId = useRef(null);
 
   const [userLat, setUserLat] = useState("");
   const [userLong, setUserLong] = useState("");
   const [recDH, setRecDH] = useState("");
   const [finishRec, setFinishRec] = useState(false);
   const [finishCheckGroup, setFinishCheckGroup] = useState(false);
+  const [justEntered, setJustEntered] = useState(true);
+
+  const checkIfQueued = () => {
+    const params = { NetID: userNetID };
+    const checkGroupURL = new URL("http://localhost:3000/dev/checkGroup");
+    checkGroupURL.search = new URLSearchParams(params).toString();
+
+    fetch(checkGroupURL, {
+      headers: {
+        Authorization: encodeBasicAuthHeader("Google", userTokenID),
+      },
+    })
+      .then((response) => response.json())
+      .then(function (response) {
+        if (response.message === "Success!") {
+          if (justEntered) {
+            setFinishCheckGroup(true);
+            setJustEntered(false);
+          }
+
+          return response;
+        }
+
+        return Promise.reject();
+      })
+      .then(({ message, ...rest }) => {
+        setQueueReqResponse(rest);
+      })
+      .finally(() => {
+        queueReqTimeoutId.current = setTimeout(checkIfQueued, 10000);
+      });
+  };
+
+  useEffect(() => {
+    checkIfQueued();
+
+    return () => {
+      clearTimeout(queueReqTimeoutId.current);
+    };
+  }, []);
 
   useEffect(() => {
     //get user location
@@ -49,51 +90,12 @@ const Queue = ({
       .then(({ diningHalls }) => {
         setDiningHalls(diningHalls.map(({ DiningHallName }) => DiningHallName));
       });
-
-    // check for a previous request or if someone has queued you up in a group
-
-    var params = { NetID: userNetID };
-    var checkGroupURL = new URL("http://localhost:3000/dev/checkGroup");
-    checkGroupURL.search = new URLSearchParams(params).toString();
-
-    fetch(checkGroupURL, {
-      headers: {
-        Authorization: encodeBasicAuthHeader("Google", userTokenID),
-      },
-    })
-      .then((response) => response.json())
-      .then(function (r) {
-        if (r.message === "Success!") {
-          setFinishCheckGroup(true);
-          return r;
-        } else {
-          console.log("You are not in a group");
-          return Promise.reject();
-        }
-      })
-      .then(({ queueRequest }) => {
-        /* this populates the queueRequest state */
-        var toFilter = [
-          "QueueRequestID",
-          "DiningHallName",
-          "AdmitOffQueueTime",
-          "QueueGroup",
-        ];
-        setQueueReqResponse(
-          Object.keys(queueRequest)
-            .filter((key) => toFilter.includes(key))
-            .reduce((obj, key) => {
-              obj[key] = queueRequest[key];
-              return obj;
-            }, {})
-        );
-      });
   }, []);
 
   useEffect(() => {
     if (selectedDiningHall === "") return;
 
-    clearTimeout(queueTimeoutId);
+    clearTimeout(queueSizeTimeoutId.current);
 
     const url = new URL("http://localhost:3000/dev/queue/size");
     url.search = new URLSearchParams({ DiningHallName: selectedDiningHall });
@@ -108,11 +110,15 @@ const Queue = ({
         .then((res) => res.json())
         .then(({ queueSize }) => {
           setQueueSize(queueSize);
-          setQueueTimeoutId(setTimeout(pollQueueSize, 30000));
+          queueSizeTimeoutId.current = setTimeout(pollQueueSize, 30000);
         });
     };
 
     pollQueueSize();
+
+    return () => {
+      clearTimeout(queueSizeTimeoutId.current);
+    };
   }, [selectedDiningHall]);
 
   const handleSelect = (event) => {
@@ -195,25 +201,22 @@ const Queue = ({
       {queueReqResponse &&
         Object.keys(queueReqResponse).length !== 0 &&
         hasLoggedIn && (
-          <>
-            <hr style={{ width: "80%" }}></hr>
-            <Box
-              display="flex"
-              alignItems="center"
-              width="100%"
-              height="350px"
-              justifyContent="center"
-            >
-              <QueueDisplay
-                queueReqResponse={queueReqResponse}
-                setQueueReqResponseCB={setQueueReqResponse}
-                userTokenID={userTokenID}
-                userNetID={userNetID}
-              />
-            </Box>
-          </>
+          <Box
+            display="flex"
+            alignItems="center"
+            width="100%"
+            height="350px"
+            justifyContent="center"
+            marginTop="100px"
+          >
+            <QueueDisplay
+              queueReqResponse={queueReqResponse}
+              setQueueReqResponseCB={setQueueReqResponse}
+              userTokenID={userTokenID}
+              userNetID={userNetID}
+            />
+          </Box>
         )}
-
       {finishRec && (
         <Snackbar open={finishRec} onClose={handleClose}>
           <Alert onClose={handleClose} severity="success">
