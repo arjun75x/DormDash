572118710cdeedd39f7ac31c81f291c2460cddb3
Bailey Tincher <btincher99@gmail.com>
2020-08-05 19:02:44 -0500

    remove title

--- frontend/src/queue/queueDisplay/AdmittedDisplay/index.jsx
+++ frontend/src/queue/queueDisplay/AdmittedDisplay/index.jsx
@@ -62,9 +62,9 @@ const AdmittedDisplay = ({
         <Typography variant="body2" component="p">
           {Date(admittedEntry.AdmitOffQueueTime)}
         </Typography>
-        <Typography className={classes.title} gutterBottom>
+        {/* <Typography className={classes.title} gutterBottom>
           Queue Group
-        </Typography>
+        </Typography> */}
         {/* <Typography variant="body2" component="p">
           {admittedEntry.QueueGroup.join(", ")}
         </Typography> */}
